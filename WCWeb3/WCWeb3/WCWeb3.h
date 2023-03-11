c1e5ce2403fe8c3dae7b26156253f54b4d5489b7
Tony <vungoc.dang@yum.com>
2023-03-11 17:12:07 +0700

    init

---
+++ WCWeb3/WCWeb3/WCWeb3.h
@@ -0,0 +1,18 @@
+//
+//  WCWeb3.h
+//  WCWeb3
+//
+//  Created by mbpro on 11/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WCWeb3.
+FOUNDATION_EXPORT double WCWeb3VersionNumber;
+
+//! Project version string for WCWeb3.
+FOUNDATION_EXPORT const unsigned char WCWeb3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WCWeb3/PublicHeader.h>
+
+
